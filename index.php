9e397ff37a1c8d18401fdd1df467bcfabeb995f5
chapsjust <2238424@carrefour.cegepvicto.ca>
2024-03-20 17:04:55 -0400

    navbar JC

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Portfolio</title>
+    <link rel="stylesheet" href="./Assets/css/style.css">
+    <script src="script.js"></script>
 </head>
 <body>
-    <h1>Portfolio</h1>
+    <nav class="navbar">
+        <div class="navbar_nom"><h1>Mon Portfolio</h1></div>
+        <ul class="navbar-nav">
+            <li class="nav-item">Accueil</li>
+            <li class="nav-item">Projets</li>
+            <li class="nav-item">À propos</li>
+            <ul class="submenu">
+                <li class="nav-item-sub">Moi Justin</li>
+                <li class="nav-item-sub">Mon Cv</li>
+                <li class="nav-item-sub">Compétences</li>
+            </ul>
+        </ul>
+    </nav>
 </body>
 </html>
