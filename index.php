5cd6f184bb094465b8559bbeecbbf33abed33a35
chapsjust <2238424@carrefour.cegepvicto.ca>
2024-03-22 15:43:24 -0400

    3/22/2024

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Portfolio</title>
     <link rel="stylesheet" href="./Assets/css/style.css">
     <script src="script.js"></script>
+    <script src="https://kit.fontawesome.com/16ddabd7d0.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar">
-        <div class="navbar_nom"><h1>Mon Portfolio</h1></div>
-        <ul class="navbar-nav">
-            <li class="nav-item">Accueil</li>
-            <li class="nav-item">Projets</li>
-            <li class="nav-item">À propos</li>
-            <ul class="submenu">
-                <li class="nav-item-sub">Moi Justin</li>
-                <li class="nav-item-sub">Mon Cv</li>
-                <li class="nav-item-sub">Compétences</li>
-            </ul>
-        </ul>
-    </nav>
+    <?php include('./assets/include/header.inc'); ?>
+    <main>
+        <div class="main-container">
+            <h1>Bienvenue à mon Portfolio !</h1>
+            <p>Mon nom est Justin Chaput étudiant en informatique passionnée de l'informatique</p>
+            <p>Je suis actuellement en formation pour devenir technicien en informatique</p>
+            <p>Se site va démontrer mes capcitées en develeppement Web</p>
+            <p>Je vous invite à visiter mes projets et à me contacter pour plus d'informations</p>
+            <img src="" alt="Justin">
+        </div>
+    </main>
+    <?php include('./assets/include/footer.inc'); ?>
 </body>
 </html>
