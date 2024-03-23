3e0d0cbbc51c261f034c5381cddf2509116c6baf
ebeliveau01 <2238950@carrefour.cegepvicto.ca>
2024-03-23 13:53:45 -0400

    Tes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <p>Mon nom est Justin Chaput étudiant en informatique passionnée de l'informatique</p>
             <p>Je suis actuellement en formation pour devenir technicien en informatique</p>
             <p>Se site va démontrer mes capcitées en develeppement Web</p>
-            <p>Je vous invite à visiter mes projets et à me contacter pour plus d'informations</p>
+            <p>Je vous invite à visiter mes projets et à me contacter pour plus d'informations s</p>
             <img src="" alt="Justin">
         </div>
     </main>
